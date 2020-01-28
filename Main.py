b8eab370172133f5ed184681c55a901f8ed89287
astyd256 <astyd637@gmail.com>
2020-01-28 19:51:29 +0700

    Add random genetarion Still need to fix it. Because the generation is fully random it takes plenty of time to generate ships

--- Main.py
+++ Main.py
@@ -1,21 +1,37 @@
 import sys
 
 from PySide2 import QtWidgets
-
+import random
 
 class Game (QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
         # Window preferences
-        self.setWindowTitle("Морской Бой v0.0.4 pre alpha")
+        self.setWindowTitle("Морской Бой v0.1.0 pre alpha")
         self.setFixedSize(800, 600)
 
         # Initializing some parameters
         self.player_ships = [4, 3, 2, 1]
+        self.enemy_ships = [[0 for i in range(10)] for j in range(10)]
         self.player_grid = {}
         self.enemy_grid = {}
-        self.grid_create()
+
+        for y in range(10):
+            for x in range(10):
+                self.player_grid[(y, x)] = QtWidgets.QPushButton('', self)
+                self.player_grid[(y, x)].setGeometry(10, 10, 20, 20)
+                self.player_grid[(y, x)].move(60 + y * 30, 40 + x * 30)
+                self.player_grid[(y, x)].setStyleSheet("background-color: cyan")
+
+                self.enemy_grid[(y, x)] = QtWidgets.QPushButton('', self)
+                self.enemy_grid[(y, x)].setGeometry(10, 10, 20, 20)
+                self.enemy_grid[(y, x)].move(440 + y * 30, 40 + x * 30)
+                self.enemy_grid[(y, x)].setStyleSheet("background-color: cyan")
+                self.enemy_grid[(y, x)].setDisabled(True)
+
+                self.player_grid[(y, x)].clicked.connect(self.player_click)
+                self.enemy_grid[(y, x)].clicked.connect(self.enemy_click)
 
         # Clear button
         self.clear_button = QtWidgets.QPushButton('&Clear', self)
@@ -27,69 +43,54 @@ class Game (QtWidgets.QWidget):
         self.start_button.clicked.connect(self.start)
         self.start_button.move(350, 440)
 
-    def grid_create(self):
-        for i in range(10):
-            for j in range(10):
-                self.player_grid[(i, j)] = QtWidgets.QPushButton('', self)
-                self.player_grid[(i, j)].setGeometry(10, 10, 20, 20)
-                self.player_grid[(i, j)].move(60+j*30, 40+i*30)
-                self.player_grid[(i, j)].setStyleSheet("background-color: cyan")
-
-                self.enemy_grid[(i, j)] = QtWidgets.QPushButton('', self)
-                self.enemy_grid[(i, j)].setGeometry(10, 10, 20, 20)
-                self.enemy_grid[(i, j)].move(440 + j * 30, 40 + i * 30)
-                self.enemy_grid[(i, j)].setStyleSheet("background-color: cyan")
-                self.enemy_grid[(i, j)].setDisabled(True)
-
-                self.player_grid[(i, j)].clicked.connect(self.player_click)
-                self.enemy_grid[(i, j)].clicked.connect(self.enemy_click)
-
     def player_click(self):
+        """Handler of creating player’s grid"""
         # Need to add a layout probably to scale buttons but unknown
         # how to get a name from buttons after that change
 
         # Taking back name of a button
         # and translate it by fixed formula
-        i = (int(self.sender().y()) - 40)/30
-        j = (int(self.sender().x()) - 60)/30
+        y = (int(self.sender().y()) - 40)//30
+        x = (int(self.sender().x()) - 60)//30
 
-        if self.player_grid[abs(i - 1), abs(j - 1)].styleSheet() == "background-color: red":
+        if self.player_grid[abs(y - 1), abs(x - 1)].styleSheet() == "background-color: red":
             return
-        if i < 9 and j < 9 and self.player_grid[(i + 1, j + 1)].styleSheet() == "background-color: red":
+        if y < 9 and x < 9 and self.player_grid[(y + 1, x + 1)].styleSheet() == "background-color: red":
             return
-        if i < 9 and self.player_grid[(i + 1, abs(j - 1))].styleSheet() == "background-color: red":
+        if y < 9 and self.player_grid[(y + 1, abs(x - 1))].styleSheet() == "background-color: red":
             return
-        if j < 9 and self.player_grid[(abs(i - 1), j + 1)].styleSheet() == "background-color: red":
+        if x < 9 and self.player_grid[(abs(y - 1), x + 1)].styleSheet() == "background-color: red":
             return
 
-        k = i - 1
+        # Counter of OTHER red squares
+        k = y - 1
         counter = 0
-        while k > -1 and self.player_grid[(k, j)].styleSheet() == "background-color: red":
+        while k > -1 and self.player_grid[(k, x)].styleSheet() == "background-color: red":
             counter += 1
             k -= 1
-        k = j - 1
-        while k > -1 and self.player_grid[(i, k)].styleSheet() == "background-color: red":
+        k = x - 1
+        while k > -1 and self.player_grid[(y, k)].styleSheet() == "background-color: red":
             counter += 1
             k -= 1
-        k = i + 1
-        while k < 10 and self.player_grid[(k, j)].styleSheet() == "background-color: red":
+        k = y + 1
+        while k < 10 and self.player_grid[(k, x)].styleSheet() == "background-color: red":
             counter += 1
             k += 1
-        k = j + 1
-        while k < 10 and self.player_grid[(i, k)].styleSheet() == "background-color: red":
+        k = x + 1
+        while k < 10 and self.player_grid[(y, k)].styleSheet() == "background-color: red":
             counter += 1
             k += 1
 
         # Checks if there an active square or not
         # If not, active square and change counter
-        if self.player_grid[(i, j)].styleSheet() == "background-color: cyan":
+        if self.player_grid[(y, x)].styleSheet() == "background-color: cyan":
             if self.player_ships[counter] > 0:
                 if counter > 0:
                     self.player_ships[counter - 1] += 1
                     self.player_ships[counter] -= 1
                 elif counter == 0:
                     self.player_ships[counter] -= 1
-                self.player_grid[(i, j)].setStyleSheet("background-color: red")
+                self.player_grid[(y, x)].setStyleSheet("background-color: red")
         # If is disable this square and change counter
         # TODO: Add a log to display exception error
         # TODO: Fix bug when you try divide 4, 3 or 2-squared ship
@@ -101,25 +102,35 @@ class Game (QtWidgets.QWidget):
                 elif self.player_ships[counter - 1] > 0:
                     self.player_ships[counter - 1] -= 1
                     self.player_ships[counter] += 1
-                    self.player_grid[(i, j)].setStyleSheet("background-color: cyan")
+                    self.player_grid[(y, x)].setStyleSheet("background-color: cyan")
             elif counter == 0:
                 self.player_ships[counter] += 1
-                self.player_grid[(i, j)].setStyleSheet("background-color: cyan")
+                self.player_grid[(y, x)].setStyleSheet("background-color: cyan")
 
     def enemy_click(self):
+        """
+        Handler of player’s clicks on enemy grid
+        """
+        # TODO: Create handler
         btn = self.sender()
-
-        btn.setStyleSheet("background-color: red")
         btn.setDisabled(True)
 
     def clear(self):
-        for i in range(10):
-            for j in range(10):
-                self.player_grid[(i, j)].setStyleSheet("background-color: cyan")
-                self.player_grid[(i, j)].setDisabled(False)
+        """
+        Clear button
+        """
+        for y in range(10):
+            for x in range(10):
+                self.player_grid[(y, x)].setStyleSheet("background-color: cyan")
+                self.player_grid[(y, x)].setDisabled(False)
+        self.player_ships = [4, 3, 2, 1]
 
     def start(self):
-        self.generate_enemy()
+        """
+        Start button
+        """
+        # TODO: fix start button
+        self.generate_enemy_map()   
         self.clear_button.setDisabled(True)
         self.start_button.setDisabled(True)
         for i in range(10):
@@ -127,9 +138,54 @@ class Game (QtWidgets.QWidget):
                 self.player_grid[(i, j)].setDisabled(True)
         self.game_loop()
 
-    def generate_enemy(self):
+    def generate_enemy_map(self):
+        """
+        Generates random enemy ships
+        and write them down in self.enemy_ships
+        """
         # TODO: Create enemy generation
-        pass
+        # TODO: Create list of available squares
+        enemy_ships = 10
+        k = 4
+        check = True
+        while enemy_ships > 0:
+            axis = random.choice('xy')
+            y = random.randrange(0, 10 - k*(axis == 'y'))
+            x = random.randrange(0, 10 - k*(axis == 'x'))
+
+            # square checking
+            for s in range(k):
+                if self.enemy_ships[y + s * (axis == 'y')][x + s * (axis == 'x')] != 0:
+                    check = False
+                    break
+
+            if check:
+                '''
+                s - step
+                k - number of available squares
+                
+                Because I know in what direction ship is building I can block the previous square.
+                Even if it's out of range abs saving the day.
+                Moreover it will work for any size of ship because it will either rewritten to 1
+                in enemy map or be on next square after the ship.  
+                '''
+                for s in range(k):
+                    self.enemy_ships[y + s*(axis == 'y')][x + s*(axis == 'x')] = 1
+                    # TODO: make algorithm that checks neighbor squares
+                    for j in (y+s*(axis == 'y') - 1, y+s*(axis == 'y'), y+s*(axis == 'y')+1):
+                        if -1 < j < 10:
+                            for i in (x+s*(axis == 'x') - 1, x+s*(axis == 'x'), x+s*(axis == 'x')+1):
+                                if -1 < i < 10:
+                                    if self.enemy_ships[j][i] == 0:
+                                        self.enemy_ships[j][i] = 2
+
+                enemy_ships -= 1
+                if enemy_ships in (9, 7, 4):
+                    k -= 1
+
+        '''Debug'''
+        for line in self.enemy_ships:
+            print(line)
 
     def game_loop(self):
         # TODO: Create game loop
